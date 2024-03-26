d46bfebfd477226a8badaa00d225b7531c89a73a
justin <kinhanjustin@gmail.com>
2024-03-26 15:12:50 +0100

    home pagina html

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ function toggleDropdown() {
         dropdownContent.style.display = "none";
     }
 }
+
+function toggleDropdown2() {
+    var dropdownContent = document.getElementById("dropdownContent2");
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+}
+function toggleDropdown3() {
+    var dropdownContent = document.getElementById("dropdownContent3");
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+}
